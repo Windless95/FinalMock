4433af04c0f9b8dead21025f7c8cab935f3b15d7
joel barrera <87788313+Windless95@users.noreply.github.com>
2022-09-06 16:14:02 -0400

    Done

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector("button.mobile-menu-button-close");
+let btn1 = document.querySelector("button.mobile-menu-button-open");
+let menu = document.querySelector('.mobile-menu');
+let desk = document.querySelector('.desktop-menu')
+let desk1 = document.querySelector("button.desktop-menu-button")
+
+
+
+//event listener for the mobile menu to open.
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('hidden')
+} )
+
+btn1.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
+
+desk1.addEventListener('click', () => {
+    desk.classList.toggle('hidden')
+})
+
+
+
